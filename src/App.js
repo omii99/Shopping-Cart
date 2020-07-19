033a97e5a723b964c8b40c5f40c3efd7ac5be04a
Mandara Sharannya <mandarasharannya990116@gmail.com>
2020-07-19 10:01:50 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import data from "./data.js";
 
+import "./App.css";
+import ProductScreen from "./Screens/ProductScreen.js";
+import HomeScreen from "./Screens/HomeScreen.js";
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 function App() {
+  const openMenu = () => {
+    document.querySelector(".sidebar").classList.add("open");
+  };
+  const closeMenu = () => {
+    document.querySelector(".sidebar").classList.remove("open");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="header">
+          <div className="brand">
+            <button onClick={openMenu}>&#9776;</button>
+            <Link to="/" >Shopping Cart</Link>
+
+          </div>
+          <div className="header-links">
+            <a href="cart.html">Cart</a>
+
+          </div>
+        </header>
+        <aside className="sidebar">
+          <h3>Shopping Categories</h3>
+          <button className="sidebar-close-button" onClick={closeMenu}>
+            x
+        </button>
+          <ul>
+            <li>
+              <a href="index.html">Men</a>
+            </li>
+
+            <li>
+              <a href="index.html">Women</a>
+            </li>
+            <li>
+              <a href="index.html">Kids</a>
+            </li>
+          </ul>
+        </aside>
+        <main className="main">
+          <div className="content">
+            <Route path="/products/:id" component={ProductScreen} />
+            <Route path="/" exact={true} component={HomeScreen} />
+
+          </div>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,34 @@
+export default {
+  products: [
+    {
+      _id: '1',
+      name: "slim shirt",
+      catrgory: "shirts",
+      image: "/images/d1.jpg",
+      brand: "Polo",
+      price: 30,
+      rating: 4.5,
+      numReviews: 10,
+    },
+    {
+      _id: '2',
+      name: "slim shirt",
+      catrgory: "shirts",
+      image: "/images/d1.jpg",
+      brand: "Polo",
+      price: 40,
+      rating: 4.5,
+      numReviews: 10,
+    },
+    {
+      _id: '3',
+      name: "slim shirt",
+      catrgory: "Tshirts",
+      image: "/images/d1.jpg",
+      brand: "Polo",
+      price: 50,
+      rating: 4.5,
+      numReviews: 10,
+    },
+  ]
+}
